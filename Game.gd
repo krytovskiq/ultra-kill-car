342b2e1e1a71011b37e5f29954b788b5b3fa957d
Krytovskiq <krytovskiq@gmail.com>
2026-05-07 23:12:44 +0500

    z

--- Game.gd
+++ Game.gd
@@ -5,7 +5,7 @@ var car_data = [
 	{"path": "res://cars/Doge_Optimizado/Doge2.tscn", "price": 0},
 	{"path": "res://cars/BTR/btr.tscn", "price": 0}
 ]
-var money: int = 10000
+var money: int = 0
 var selected_car_index: int = 0
 var owned_cars: Array = [0] # Индекс 0 (первая машина) куплен сразу
 
@@ -14,7 +14,7 @@ func save_data():
 	file.store_var(money)
 	file.store_var(selected_car_index)
 	file.store_var(owned_cars)
-
+	
 func load_data():
 	if FileAccess.file_exists("user://save.dat"):
 		var file = FileAccess.open("user://save.dat", FileAccess.READ)
